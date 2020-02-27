b235ea0a07efe06e2684e72dac721c6c9e216653
Andrew <aschult5@gmail.com>
2020-02-27 11:23:27 -0800

    Correct Message documentation typo.
    This is cleanup from #11

--- message.go
+++ message.go
@@ -1,6 +1,6 @@
 package action
 
-// ActionMessage defines the schema for messages passed to AddAction.
+// Message defines the schema for messages passed to AddAction.
 type Message struct {
 	Action *string
 	Time   *float64
