6b0ae1a20b436c7ea0895d6911cb7e51e64873a2
Andrew <aschult5@gmail.com>
2020-02-27 14:08:38 -0800

    Add TestImplAddAndGet, the first functional test

--- statsimpl_test.go
+++ statsimpl_test.go
@@ -2,21 +2,33 @@ package action
 
 import "testing"
 
-func TestImplAddAction(t *testing.T) {
-	var impl statsImpl
-	action := "jump"
-	time := 100.0
-
-	msg := InputMessage{&action, &time}
-
-	impl.addAction(msg)
-}
-
 func TestImplGetStats(t *testing.T) {
 	var impl statsImpl
 
 	stats := impl.getStats()
 	if len(stats) != 0 {
 		t.Errorf("Expected empty stats, not %v", stats)
+	}
+}
+
+func TestImplAddAndGet(t *testing.T) {
+	var impl statsImpl
+
+	// Form InputMessage
+	action := "jump"
+	time := 100.0
+	msg := InputMessage{&action, &time}
+
+	// Verify action was added
+	impl.addAction(msg)
+	stats := impl.getStats()
+	if len(stats) != 1 {
+		t.Errorf("Expected stats with 1 entry, not %v", stats)
+	} else {
+		// Verify OutputMessage matches expected
+		expected := OutputMessage{action, time}
+		if stats[0] != expected {
+			t.Errorf("%v did not match expected %v", stats[0], expected)
+		}
 	}
 }
