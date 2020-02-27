35705a3ea836ed74b8b6fd3324937499d4b93d5e
Andrew <aschult5@gmail.com>
2020-02-27 11:29:51 -0800

    Introduce Average type.
    This type will maintain a running average through its only method: Add.
    
    This resolves #3

---
+++ average.go
@@ -0,0 +1,14 @@
+package action
+
+// Average maintains a running average by tracking the count of entries
+type Average struct {
+	Value float64 `json:"avg"`
+	Count uint64  `json:"-"`
+}
+
+// Add updates the average as Avg[n] = Avg[n-1]*(n-1)/n + Val[n]/n
+func (avg *Average) Add(val float64) {
+	avg.Value *= float64(avg.Count) / float64(avg.Count+1)
+	avg.Value += val / float64(avg.Count+1)
+	avg.Count += 1
+}
---
+++ average_test.go
@@ -0,0 +1,37 @@
+package action
+
+import "testing"
+
+func TestNaturalAverage(t *testing.T) {
+	var avg Average
+
+	avg.Add(5)
+	avg.Add(15)
+	expected := 10.0
+
+	if avg.Value != expected {
+		t.Errorf("Expected average of %f not %f", expected, avg.Value)
+	}
+}
+
+func TestRationalAverage(t *testing.T) {
+	var avg Average
+
+	avg.Add(3)
+	avg.Add(4)
+	expected := 3.5
+
+	if avg.Value != expected {
+		t.Errorf("Expected average of %f not %f", expected, avg.Value)
+	}
+}
+
+func TestEmptyAverage(t *testing.T) {
+	var avg Average
+
+	expected := 0.0
+
+	if avg.Value != expected {
+		t.Errorf("Expected average of %f not %f", expected, avg.Value)
+	}
+}
