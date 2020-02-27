095b15297ae506825d92901c0ce898fe4095528c
Andrew <aschult5@gmail.com>
2020-02-26 20:00:03 -0800

    Document ActionStat.
    This partially addresses #8

--- actionstat.go
+++ actionstat.go
@@ -1,12 +1,17 @@
 package actionstat
 
+// ActionStat tracks passed actions' average times.
 type ActionStat struct {
 }
 
+// AddAction takes json input and updates the action's time average.
+// Keys are case insensitive.
+// String values are case sensitive.
 func (a ActionStat) AddAction(json string) error {
 	return nil
 }
 
+// GetStats returns the averages of all actions as json.
 func (a ActionStat) GetStats() string {
 	return "{}"
 }
