bb03cb18cd7adb759b25794a20475f4ed4fc3da5
Andrew <aschult5@gmail.com>
2020-02-28 07:29:34 -0800

    Convert statsImpl from a map to a struct containing a map.
    This partially addresses #22

--- statsimpl.go
+++ statsimpl.go
@@ -1,23 +1,25 @@
 package action
 
 // statsImpl implements Stats
-type statsImpl map[string]Average
+type statsImpl struct {
+	average map[string]Average
+}
 
 // addAction implements Stats.AddAction
 func (impl *statsImpl) addAction(msg InputMessage) {
-	if len(*impl) == 0 {
-		*impl = make(statsImpl)
+	if len(impl.average) == 0 {
+		impl.average = make(map[string]Average)
 	}
-	avg := (*impl)[*msg.Action]
+	avg := impl.average[*msg.Action]
 	avg.Add(*msg.Time)
-	(*impl)[*msg.Action] = avg
+	impl.average[*msg.Action] = avg
 }
 
 // getStats implements Stats.GetStats
-func (impl statsImpl) getStats() []OutputMessage {
-	ret := make([]OutputMessage, 0, len(impl))
+func (impl *statsImpl) getStats() []OutputMessage {
+	ret := make([]OutputMessage, 0, len(impl.average))
 
-	for key, avg := range impl {
+	for key, avg := range impl.average {
 		ret = append(ret, OutputMessage{key, avg.Value})
 	}
 
