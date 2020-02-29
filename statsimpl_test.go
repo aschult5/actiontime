8cf67a04527eaf49755e066940c5ee4bb60e0899
Andrew <aschult5@gmail.com>
2020-02-29 10:39:38 -0800

    Move test to testdata, per `go help test`.
    This may relate to #33

--- statsimpl_test.go
+++ statsimpl_test.go
@@ -6,27 +6,27 @@ import (
 )
 
 func TestEmpty(t *testing.T) {
-	runTestCase("test/tc_empty.csv", t)
+	runTestCase("testdata/tc_empty.csv", t)
 }
 
 func TestWrOne(t *testing.T) {
-	runTestCase("test/tc_wr_one_one.csv", t)
+	runTestCase("testdata/tc_wr_one_one.csv", t)
 }
 
 func TestWrFew(t *testing.T) {
-	runTestCase("test/tc_wr_few_few.csv", t)
+	runTestCase("testdata/tc_wr_few_few.csv", t)
 }
 
 func TestWrFewAsync(t *testing.T) {
-	runTestCase("test/tc_wr_few_few_async.csv", t)
+	runTestCase("testdata/tc_wr_few_few_async.csv", t)
 }
 
 func TestBalFewAsync(t *testing.T) {
-	runTestCase("test/tc_bal_few_few_async.csv", t)
+	runTestCase("testdata/tc_bal_few_few_async.csv", t)
 }
 
 func TestWrMilAsync(t *testing.T) {
-	tc := "test/gen/tc_wr_mil_few_async.csv"
+	tc := "testdata/gen/tc_wr_mil_few_async.csv"
 	if !fileExists(tc) {
 		cmd := fmt.Sprintf("python3 tools/testgenerator.py --csv %s --add 1000000 jump run sit stand", tc)
 		t.Errorf("Please generate %s with...\n`%s`", tc, cmd)
