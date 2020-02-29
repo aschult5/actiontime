d209345cad437e6d234e441c010bf8be71166da1
Andrew <aschult5@gmail.com>
2020-02-29 11:51:59 -0800

    Share test cases across Stats and statsImpl.
    This allows for load testing on the public interface (Stats).
    Previously only statsImpl was load tested.
    
    This relates to #38

---
+++ statswrapper_test.go
@@ -0,0 +1,61 @@
+package actiontime
+
+import (
+	"fmt"
+	"testing"
+)
+
+func testStatsImpl(t *testing.T, csvFn string) {
+	t.Log("Testing statsImpl")
+	var obj statsImplWrap
+	runner := testRunner{CsvFn: csvFn, TestT: t, Obj: &obj}
+	runner.Run()
+}
+
+func testStats(t *testing.T, csvFn string) {
+	t.Log("Testing Stats")
+	var obj statsWrap
+	runner := testRunner{CsvFn: csvFn, TestT: t, Obj: &obj}
+	runner.Run()
+}
+
+func TestEmpty(t *testing.T) {
+	tc := "testdata/tc_empty.csv"
+	testStatsImpl(t, tc)
+	testStats(t, tc)
+}
+
+func TestWrOne(t *testing.T) {
+	tc := "testdata/tc_wr_one_one.csv"
+	testStatsImpl(t, tc)
+	testStats(t, tc)
+}
+
+func TestWrFew(t *testing.T) {
+	tc := "testdata/tc_wr_few_few.csv"
+	testStatsImpl(t, tc)
+	testStats(t, tc)
+}
+
+func TestWrFewAsync(t *testing.T) {
+	tc := "testdata/tc_wr_few_few_async.csv"
+	testStatsImpl(t, tc)
+	testStats(t, tc)
+}
+
+func TestBalFewAsync(t *testing.T) {
+	tc := "testdata/tc_bal_few_few_async.csv"
+	testStatsImpl(t, tc)
+	testStats(t, tc)
+}
+
+func TestWrMilAsync(t *testing.T) {
+	tc := "testdata/gen/tc_wr_mil_few_async.csv"
+	if !fileExists(tc) {
+		cmd := fmt.Sprintf("python3 tools/testgenerator.py --csv %s --add 1000000 jump run sit stand", tc)
+		t.Errorf("Please generate %s with...\n`%s`", tc, cmd)
+	} else {
+		testStatsImpl(t, tc)
+		testStats(t, tc)
+	}
+}
